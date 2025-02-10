ac1320786ba284fa6f17b2129fe69d04397e5954
fabianosfbr <contato@fabianofernandes.adm.br>
2025-02-10 12:03:57 -0300

    wip: adiciona comando para recuperar acumulador da dominio

---
+++ app/Console/Commands/ImportAcumulador.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Console\Commands;
+
+use App\Models\Cliente;
+use App\Models\Empresa;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ImportAcumulador extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:acumulador';
+
+    protected $description = 'Import acumuladores from Dominio ODBC to Fiscaut Connector';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $tableName = 'bethadba.efacumuladores';
+
+        $empresas = Empresa::where('sync', true)
+            ->where('cliente', true)
+            ->get();
+
+        foreach ($empresas as $empresa) {
+            $rows = DB::connection('odbc')
+                ->table($tableName)
+                ->where('codi_emp', $empresa->codi_emp)
+                ->orderBy('codi_cli', 'desc')
+                ->get();
+
+            foreach ($rows as $key => $row) {
+                dd($row);
+                $row->nome_cli = removeCaracteresEspeciais($row->nome_cli);
+
+                $this->info('Cliente: '.$row->nome_cli.' CNPJ/CPF: '.$row->cgce_cli);
+
+                Cliente::updateOrCreate(
+                    [
+                        'codi_emp' => $row->codi_emp,
+                        'codi_cli' => $row->codi_cli,
+                    ],
+                    [
+                        'codi_emp' => $row->codi_emp,
+                        'codi_cli' => $row->codi_cli,
+                        'nome_cli' => $row->nome_cli,
+                        'cgce_cli' => $row->cgce_cli,
+                        'codi_cta' => $row->codi_cta,
+                    ]
+                );
+            }
+        }
+    }
+}
